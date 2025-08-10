e01f16ea7daf1dfe3685ecb52dae8a2d89ec0e1e
Jay Solanki <jaysol8900@gmail.com>
2025-08-11 00:47:53 +0530

    multer service implemented

---
+++ backend/src/service/multer.config.js
@@ -0,0 +1,13 @@
+const multer = require("multer");
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    fileFilter: (req, file, cb) => {
+        if (!file.mimetype.startsWith("image/")) {
+            return cb(new Error("Only image files are allowed!"), false);
+        }
+        cb(null, true);
+    }
+});
+
+module.exports = upload;
